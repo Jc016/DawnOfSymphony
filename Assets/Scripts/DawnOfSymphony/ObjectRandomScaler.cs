93d3dfe66fd93d53e42728914d559c41afb8e104
Jc016 <jclm1992@gmail.com>
2017-04-07 17:59:18 -0400

    all objects integrated

---
+++ Assets/Scripts/DawnOfSymphony/ObjectRandomScaler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRandomScaler : MonoBehaviour {
+
+    // Use this for initialization
+
+    public float minRange = 0f, maxRange = 1.0f;
+
+
+     void Awake(){
+        SetRandomScale();
+    }
+    void Start () {
+		
+	}
+
+    void SetRandomScale(){
+        float scalingFactor = Random.Range(minRange, maxRange);
+        transform.localScale = new Vector3(scalingFactor, scalingFactor, scalingFactor);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
