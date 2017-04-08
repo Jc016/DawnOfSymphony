085623e6aaef7129919811d07a3c2216008aab3d
Jc016 <jclm1992@gmail.com>
2017-04-07 21:19:39 -0400

    optimisation

---
+++ Assets/Scripts/DawnOfSymphony/ObjectRandomRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRandomRotation : MonoBehaviour {
+
+    private void Awake()
+    {
+        transform.Rotate(Vector3.up, Random.Range(0, 360));
+    }
+
+}
